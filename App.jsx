d8f8b034eadbd5432d6cd3afed2d3d2e88a45791
shreyas399 <129728792+shreyas399@users.noreply.github.com>
2024-07-29 18:32:41 +0530

    Password generator
    Used usetates, useref ,useEffects hooks to make a password generator along with the JS ,HTML, CSS and tailwind CSS

--- App.jsx
+++ App.jsx
@@ -1,42 +1,105 @@
-import { useState } from 'react'
-
+import { useState, useCallback, useEffect,useRef } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [color, useColor]=useState('olive')
-  
-    function changeColor(color)
-    {
-      
-      useColor(color)
-    }
-  return (
-    
+  const[password, setPassword]=useState("")
+  const[number, numberAllowed]=useState(false)
+  const[chare, charAllowed]=useState(false)
+  const[length,setLength]=useState(8)
 
-    <div className='w-full h-screen duration-200 flex justify-center items-center' style={{ backgroundColor: color }}>
-      <div className='fixed flex flex-wrap justify-center items-center bottom-12 inset-x-0 px-2'>
-        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'> 
-          <button onClick={()=>
-            {
-              changeColor('red')
-            }
-           }>Click </button>
-        
-          <button onClick={ ()=>
-            {
-              useColor('blue')
-            }
-           }>Click </button>
-         
-          <button onClick={()=>
-            {
-              useColor('green')
-            }
-          }>Click </button>
-          {' '}  
-        </div>
+  const passwordRef=useRef(null)
+
+ const generatePassword= useCallback(()=>
+{
+  let pass=""
+  let str="ABCDEFGHIJKLMNOPQRSTUVWHYXabcdefghijklmnopqrstuvwxyz"
+  
+
+  if(number==true)
+  {
+    str+="0123456789"
+  }
+
+  if(chare==true)
+  {
+    str+="@#$%^&*()+"
+  }
+
+
+  for(let i=0;i<length;i++)
+  {
+    const char=Math.round(Math.random()*str.length)
+    pass+=str.charAt(char)
+  }
+  setPassword(pass)
+},[number,chare,length] )
+
+useEffect( 
+  ()=>{
+    generatePassword()
+
+},[length,chare,number])
+
+  
+
+  
+  
+
+  return (
+    <div className='w-full max-w-md mx-auto shadow-md  rounded-lg px-4 py-3 my-8 bg-gray-500 text-orange-500'>
+      <h1 className='text-white text-center  my-3'>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input type="text" 
+        value={password} 
+        readOnly
+        className='outline-none w-full py-1 px-3'
+        ref={passwordRef}
+        />
+        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0' onClick={()=>
+          {
+            window.navigator.clipboard.writeText(password)
+            passwordRef.current.select()
+            
+          }
+        }>COPY</button>
       </div>
-     </div>
+      <div className='flex text-sm gap-x-2'>
+        <input type="range" min={8} max={30} value={length} className='cursor-pointer' onChange={ (e)=>{setLength(e.target.value) }}/>
+        <label htmlFor='length'>Length: {length}</label>
+
+            <div className='flex text-sm gap-x-2'>
+            <input type="Checkbox" 
+            defaultChecked={number}
+            onChange={ ()=>
+            {
+              numberAllowed( (prev)=>!prev)
+            }
+            }
+            />
+            <label htmlFor='length'>Numbers</label>
+            </div>
+
+
+                <div className='flex text-sm gap-x-2'>
+                <input type="Checkbox"
+                 
+                defaultChecked={chare}
+                onChange={()=>
+                {
+                  charAllowed((prev)=>!prev)
+                }}
+                
+                />
+                <label htmlFor='char'>Character</label>
+
+              </div>
+
+        
+      </div>
+
+    </div>
   )
 }
 
